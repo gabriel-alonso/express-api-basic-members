a90f48a9344696613aea088822d0dedd3d5b6d83
gabriel-alonso <dev.gabriel.alonso@gmail.com>
2021-05-11 23:05:40 -0300

    :sparkles: add content main and gitignore

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'Gabriel Alonso',
+    email: 'gabriel@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'João Mario',
+    email: 'joao@gmail.com',
+    status: 'inactive',
+  },
+  {
+    id: 3,
+    name: 'Maria Silva',
+    email: 'maria@gmail.com',
+    status: 'active',
+  },
+];
+
+module.exports = members;
